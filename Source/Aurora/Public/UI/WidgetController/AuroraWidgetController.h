68a5136d17b8101f2c373a1d235141fc79d61094
KonstantinNoskov <noskovjr@yandex.ru>
2024-06-21 14:22:34 +0500

    Aurora widget controller init.

---
+++ Source/Aurora/Public/UI/WidgetController/AuroraWidgetController.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "AuroraWidgetController.generated.h"
+
+UCLASS()
+class AURORA_API UAuroraWidgetController : public UObject
+{
+	GENERATED_BODY()
+};
---
+++ Source/Aurora/Public/UI/Widgets/AuroraUserWidget.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "AuroraUserWidget.generated.h"
+
+UCLASS()
+class AURORA_API UAuroraUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
