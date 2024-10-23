4566097a488dec5699b5ae53081dd69af4f9a8f7
KonstantinNoskov <noskovjr@yandex.ru>
2024-10-23 14:58:46 +0500

    Init Commit

---
+++ Source/Aurora/Public/AbilitySystem/Abilities/AuroraPassiveAbility.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "AuroraGameplayAbility.h"
+#include "AuroraPassiveAbility.generated.h"
+
+
+UCLASS()
+class AURORA_API UAuroraPassiveAbility : public UAuroraGameplayAbility
+{
+	GENERATED_BODY()
+};
