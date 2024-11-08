18b562f225e7c7d124354d73d34884ece2476c73
KonstantinNoskov <noskovjr@yandex.ru>
2024-11-08 13:58:27 +0500

    Init commit

---
+++ Source/Aurora/Public/AuroraColors.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "AuroraColors.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURORA_API UAuroraColors : public UDataAsset
+{
+	GENERATED_BODY()
+};
