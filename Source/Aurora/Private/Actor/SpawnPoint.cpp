e184e3f7739203682a385ae0438d82525c30424c
KonstantinNoskov <noskovjr@yandex.ru>
2024-12-07 17:46:04 +0500

    Created SpawnPoint

---
+++ Source/Aurora/Private/Actor/SpawnPoint.cpp
@@ -0,0 +1,40 @@
+﻿#include "Actor/SpawnPoint.h"
+
+#include "Characters/AuroraEnemy.h"
+
+
+ASpawnPoint::ASpawnPoint()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ASpawnPoint::BeginPlay()
+{
+	Super::BeginPlay();
+
+	HandleSpawnEnemy();
+}
+
+void ASpawnPoint::HandleSpawnEnemy()	
+{	
+	// Valid Check
+	if (!bShouldSpawn) return;
+	
+	// Spawn Params
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.Owner = this;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButAlwaysSpawn;
+
+	// Spawn Enemy
+	SpawnedEnemy = GetWorld()->SpawnActor<AAuroraEnemy>(EnemySpawnClass, GetActorLocation(), GetActorRotation(), SpawnParams);
+		
+	if (IsValid(SpawnedEnemy))
+	{
+		SpawnedEnemy->GetOnDeathDelegate().AddDynamic(this, &ASpawnPoint::OnSpawnedEnemyDied);
+	}
+}
+
+void ASpawnPoint::OnSpawnedEnemyDied(AActor* DeadActor)
+{	
+	bShouldSpawn = false;
+}
---
+++ Source/Aurora/Public/Actor/SpawnPoint.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SpawnPoint.generated.h"
+
+class AAuroraEnemy;
+
+UCLASS()
+class AURORA_API ASpawnPoint : public AActor
+{
+	GENERATED_BODY()
+
+public:
+
+	ASpawnPoint();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+public:
+
+	UFUNCTION(BlueprintCallable)
+	void HandleSpawnEnemy();
+
+protected:
+
+	UFUNCTION()
+	void OnSpawnedEnemyDied(AActor* DeadActor);
+
+	UPROPERTY(EditInstanceOnly, Category = "Spawn Defaults")
+	TSubclassOf<AAuroraEnemy> EnemySpawnClass;
+
+	UPROPERTY(VisibleAnywhere, Category = "Spawn Defaults")
+	TObjectPtr<AAuroraEnemy> SpawnedEnemy;
+
+	UPROPERTY(EditInstanceOnly, SaveGame, Category = "Spawn Defaults")
+	bool bShouldSpawn = true;
+
+	
+};
